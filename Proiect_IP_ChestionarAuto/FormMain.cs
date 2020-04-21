bc5ed07d1be297c7c08c5878c43185db487a8d74
Sergiu Corban <sergiu-stefan.corban@student.tuiasi.ro>
2020-04-21 12:33:55 +0300

    Added About and Exit

---
+++ Proiect_IP_ChestionarAuto/FormMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Proiect IP
+// Chestionar Auto
+// Corban Sergiu-Stefan
+// Velovici Florin-Cristian
+// 1309B
+
+namespace Proiect_IP_ChestionarAuto
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Proiect IP\nCorban Sergiu-Stefan\nVelovici Florin-Cristian\n1309B", "Despre");
+        }
+    }
+}
